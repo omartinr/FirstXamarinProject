6d2477552dcbd8e8bd813054290ec8388f3df10c
Martin Rivas <orlando.martin.rivas@intel.com>
2019-12-04 22:55:21 -0600

    Add project files.

---
+++ projectoFinal/projectoFinal/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace projectoFinal
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Gracias", "Datos obtenidos satisfactoriamente", "OK");
+            
+            nombre.Text = String.Empty;
+            primerApellido.Text = String.Empty;
+            segundoApellido.Text = String.Empty;
+            fechaNacimiento.Date = DateTime.Today;
+            correoElectronico.Text = String.Empty;
+            telefonoContacto.Keyboard = null;
+            contrasena.Text = String.Empty;
+            contrasenaReescritura.Text = String.Empty;
+        }
+
+        }
+    }
